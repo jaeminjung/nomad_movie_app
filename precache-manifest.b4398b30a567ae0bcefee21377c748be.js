d327c051ecbca488222267cc8d9392c3eb8b1452
jaeminjung <jaeminjung@hotmail.com>
2018-10-19 22:30:25 +0900

    Updates

---
+++ precache-manifest.b4398b30a567ae0bcefee21377c748be.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "d46490d6d0cb4dcb83b0",
+    "url": "/nomad_movie_app/static/js/runtime~main.d46490d6.js"
+  },
+  {
+    "revision": "b45695aee244fe5bdaaa",
+    "url": "/nomad_movie_app/static/js/main.b45695ae.chunk.js"
+  },
+  {
+    "revision": "83b3f9b8655729265301",
+    "url": "/nomad_movie_app/static/js/1.83b3f9b8.chunk.js"
+  },
+  {
+    "revision": "b45695aee244fe5bdaaa",
+    "url": "/nomad_movie_app/static/css/main.33a5f209.chunk.css"
+  },
+  {
+    "revision": "0ce4db77c75c99aa67c2f81b6ad17a86",
+    "url": "/nomad_movie_app/index.html"
+  }
+];
